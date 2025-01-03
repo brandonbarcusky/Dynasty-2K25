1f1cb21dbfc6b6ce16329497e2e523e884ca39fc
brandonbarcusky <brandonbarcusky@gmail.com>
2025-01-02 21:36:50 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,13 +1,13 @@
 /*   STEP 1   */
 export const leagueID = "1134699509730213888"; // your league ID
 export const leagueName = "Dynasty 2K25"; // your league name
-export const dues = 100; // (optional) used in template constitution page
+export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Dynasty 2K25 tells the story of a whining boy from Malvern who won 23 games and the regular championship, only to choke it all away in the end. As stress relief, this player would buy Pokemon Scalet & Violet 151 UPCs left and right from the King of Prussia GameStop. Medical experts across the nation actually argued the point that smoking 3 packs of Marlboros a day might actually be better for one's health. Just ask RFK, Jr. </p>
+  <p>Dynasty 2K25 tells the story of a whining boy from Malvern who won 23 games and the regular season championship, only to choke it all away in the end. As stress relief, this player would buy Pokemon Scalet & Violet 151 UPCs left and right from the King of Prussia GameStop. Medical experts across the nation actually argued the point that smoking 3 packs of Marlboros a day might actually be better for one's health. Just ask RFK, Jr. </p>
   <p>The worst part about this guy is that he wouldn't shut up. He defended himself for drafting two kickers, he called Brock Bowers a bad football player, and he called Aaron Jones a "random guy". The all-time low moment of the season was when he needed 28 points from his kicker to make the championship round. He then proceeded to call his opponent "lucky" after having to root for, yet again, a kicker, to score 28 points on SNF. </p>
 `;
 
